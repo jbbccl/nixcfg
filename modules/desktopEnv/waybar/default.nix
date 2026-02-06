903fb3d971af7d1cee850c6531a1460e865b8c05
lccbbj <lccbbj@example.com>
2026-02-06 21:04:58 +0800

    e

--- modules/desktopEnv/waybar/default.nix
+++ modules/desktopEnv/waybar/default.nix
@@ -6,7 +6,6 @@
 		fuzzel
 		wofi
 		swaylock 
-		mako 
 		swayidle 
 		brightnessctl#亮度调节re
 		networkmanagerapplet
@@ -17,9 +16,46 @@
 			"waybar/" = {
 				force = true;
 				recursive = true;
-				source = ./config;
+				source = ./waybar;
+			};
+		};
+		
+		services.mako.enable = true;
+		xdg.configFile = {
+			"mako/" = {
+				force = true;
+				recursive = true;
+				source = ./mako;
+			};
+		};
+		xdg.configFile = {
+			"wofi/" = {
+				force = true;
+				recursive = true;
+				source = ./wofi;
 			};
 		};
 	};
 
 }
+
+
+# services.mako.settings={
+# 	"actionable=true" = {
+# 		anchor = "bottom-left";
+# 	};
+# 	actions = true;
+# 	anchor = "bottom-right";
+# 	background-color = "#000000";
+# 	border-color = "#FFFFFF";
+# 	border-radius = 0;
+# 	default-timeout = 0;
+# 	font = "Maple Mono NF CN 10";
+# 	height = 100;
+# 	icons = true;
+# 	ignore-timeout = false;
+# 	layer = "top";
+# 	margin = 10;
+# 	markup = true;
+# 	width = 300;
+# };
