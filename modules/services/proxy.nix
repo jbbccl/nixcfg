cbb4bac95bd7eb6488da452922b258f63c52c2b9
lccbbj <1@1>
2026-03-26 00:47:52 +0800

    先这样吧

--- modules/services/proxy.nix
+++ modules/services/proxy.nix
@@ -1,16 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, username, ... }:
 {
 	services.mihomo = {
 		enable = true;
-		configFile = "/etc/mihomo/config.yaml";
+		#先这样吧
+		configFile = "/home/${username}/nixrc/static/_security/mihomo/config.yaml";
 		webui = pkgs.metacubexd;
 	};
 
-	environment.etc = {
-		"mihomo/config.yaml" = {
-			source = ./etc/mihomo/config.yaml;
-		};
-	};
+	# environment.etc = {
+	# 	"mihomo/config.yaml" = {
+	# 		source = ./etc/mihomo/config.yaml;
+	# 	};
+	# };
 	# environment.systemPackages = with pkgs; [
 	# 	dae
 	# 	daed
