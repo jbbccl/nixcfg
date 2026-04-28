484c2da4286fcd7646475dd1a8ee012f6fa9e7d2
e <e@nixos.local>
2026-04-29 02:12:51 +0800

    ee

--- modules/__modules__.nix
+++ modules/__modules__.nix
@@ -1,25 +1,26 @@
 { lib, ... }: {
-  imports = [
-    ./development/__development__.nix
-    ./services/__services__.nix
-    ./shells/__shells__.nix
-    ./virtual/__virtual__.nix
-    ./utilities/__utilities__.nix
-  ];
+	imports = [
+		./development/__development__.nix
+		./services/__services__.nix
+		./shells/__shells__.nix
+		./virtual/__virtual__.nix
+		./utilities/__utilities__.nix
+	];
 
-  options.modules = {
-    services = lib.mkEnableOption "system services (audio, networking, ssh)";
-    development = lib.mkEnableOption "development tooling (git, languages)";
-    shells = lib.mkEnableOption "shell configurations (zsh, fish, bash)";
-    virtualization = lib.mkEnableOption "virtualization (KVM, containers, appimage)";
-    utilities = lib.mkEnableOption "utilities (neovim, yazi, basic tools)";
-  };
+	# 这些 options 只是定义了, 并没有使用
+	options.modules = {
+		services.enable = lib.mkEnableOption "system services (audio, networking, ssh)";
+		development.enable = lib.mkEnableOption "development tooling (git, languages)";
+		shells.enable = lib.mkEnableOption "shell configurations (zsh, fish, bash)";
+		virtualization.enable = lib.mkEnableOption "virtualization (KVM, containers, appimage)";
+		utilities.enable = lib.mkEnableOption "utilities (neovim, yazi, basic tools)";
+	};
 
-  config = {
-    modules.services = lib.mkDefault true;
-    modules.development = lib.mkDefault true;
-    modules.shells = lib.mkDefault true;
-    modules.virtualization = lib.mkDefault true;
-    modules.utilities = lib.mkDefault true;
-  };
+	config = {
+		modules.services.enable = lib.mkDefault true;
+		modules.development.enable = lib.mkDefault true;
+		modules.shells.enable = lib.mkDefault true;
+		modules.virtualization.enable = lib.mkDefault true;
+		modules.utilities.enable = lib.mkDefault true;
+	};
 }
