1a62d0f688d8305b6d83e0d192f97a6787c13c9b
lccbbj <lccbbj@example.com>
2026-05-03 08:37:43 +0800

    e

--- apps/gui/misc.nix
+++ apps/gui/misc.nix
@@ -3,38 +3,38 @@ let
 #   myClashVerge = pkgs.callPackage ./clash-verge/package.nix { };
 in
 {
-home-manager.users.${username} = {
-	home.packages = with pkgs; [
-		# flclash
-		# daed
+	home-manager.users.${username} = {
+		home.packages = with pkgs; [
+			# flclash
+			# daed
 
-		kdePackages.filelight
-		gparted
+			kdePackages.filelight
+			# gparted
+			kdePackages.partitionmanager
 
-		localsend
+			localsend
 
-		# 编辑器
-		imhex
-		gimp
-		zed-editor
-		# obsidian
-		libreoffice-qt
+			# 编辑器
+			imhex
+			gimp
+			zed-editor
+			# obsidian
+			libreoffice-qt
 
-		# 流
-		obs-studio
-		showmethekey
-		moonlight-qt
-		vlc
+			# 流
+			obs-studio
+			showmethekey
+			moonlight-qt
+			vlc
 
-		# 应用
-		gearlever
-		# bottles
+			# 应用
+			gearlever
+			# bottles
 
-		# 其他
-		pomodoro-gtk
-		# keepassxc
-		xeyes
-	];
-};
-
+			# 其他
+			pomodoro-gtk
+			# keepassxc
+			xeyes
+		];
+	};
 }
