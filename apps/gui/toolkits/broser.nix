2930374d7fbc5d319e6ca07016c52a722703e329
e <e@nixos.local>
2026-05-10 20:54:55 +0800

    o

--- apps/gui/toolkits/broser.nix
+++ apps/gui/toolkits/broser.nix
@@ -1,16 +1,18 @@
 { config, lib, pkgs, ... }:
 let
-	browser = if config.apps.gui.defaultBrowser or "chromium" == "firefox" then "firefox.desktop" else "chromium-browser.desktop";
+	browser = "librewolf.desktop";
 in {
 	programs.firefox.enable = true;
 	environment.systemPackages = with pkgs; [
 		ungoogled-chromium
+		# librewolf
+		floorp-bin
 	];
-	xdg.mime.defaultApplications = {
-		"text/html" = browser;
-		"x-scheme-handler/http" = browser;
-		"x-scheme-handler/https" = browser;
-		"x-scheme-handler/about" = browser;
-		"x-scheme-handler/unknown" = browser;
-	};
+	# xdg.mime.defaultApplications = {
+	# 	"text/html" = browser;
+	# 	"x-scheme-handler/http" = browser;
+	# 	"x-scheme-handler/https" = browser;
+	# 	"x-scheme-handler/about" = browser;
+	# 	"x-scheme-handler/unknown" = browser;
+	# };
 }
