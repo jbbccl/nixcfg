b7156d098794a45752e00ce095bad62ba5110378
e <e@nixos.local>
2026-05-25 13:14:14 +0800

    e

--- desktop/dispMgr/greetd/greetd.nix
+++ desktop/dispMgr/greetd/greetd.nix
@@ -1,19 +1,7 @@
 { config, lib, pkgs, ... }:
 let
 	cfg = config.desktop.dispMgr.greetd;
-	uwsmCompositors = lib.attrNames config.programs.uwsm.waylandCompositors;
 	sessionData = config.services.displayManager.sessionData.desktops;
-	uwsmSessions = pkgs.runCommand "uwsm-sessions" {} (''
-		mkdir -p $out/share/wayland-sessions $out/share/xsessions
-	''
-	+ lib.concatMapStrings (name: ''
-		if [ -f ${sessionData}/share/wayland-sessions/${name}.desktop ]; then
-			cp ${sessionData}/share/wayland-sessions/${name}.desktop $out/share/wayland-sessions/
-		fi
-		if [ -f ${sessionData}/share/xsessions/${name}.desktop ]; then
-			cp ${sessionData}/share/xsessions/${name}.desktop $out/share/xsessions/
-		fi
-	'') uwsmCompositors);
 in
 {
 	options.desktop.dispMgr.greetd.enable = lib.mkEnableOption "greetd";
@@ -26,7 +14,7 @@ in
 				default_session = {
 					command = ''
 					${lib.getExe pkgs.tuigreet} \
-					--sessions ${uwsmSessions}/share/wayland-sessions:${uwsmSessions}/share/xsessions \
+					--sessions ${sessionData}/share/wayland-sessions:${sessionData}/share/xsessions \
 					--time \
 					--time-format '%Y-%m-%d %H:%M' \
 					--asterisks \
