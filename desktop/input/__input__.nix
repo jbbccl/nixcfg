3e5adbf84c39d93293a7937348c021074859364e
e <e@nixos.local>
2026-05-26 19:39:56 +0800

    ibus在niri下异常 好像是input-method-unstable-v1支持有限

--- desktop/input/__input__.nix
+++ desktop/input/__input__.nix
@@ -6,16 +6,18 @@ in
 	imports = [
 		./rime/rime.nix
 		./fcitx5/fcitx5.nix
+		./ibus/ibus.nix
 	];
 
 	options.desktop.input.select = lib.mkOption {
-		type = lib.types.nullOr (lib.types.enum [ "fcitx5" ]);
+		type = lib.types.nullOr (lib.types.enum [ "fcitx5" "ibus" ]);
 		default = null;
 		description = "input method framework";
 	};
 
 	config = lib.mkIf (config.desktop.input.select != null) {
-        desktop.input.rime.enable = lib.mkDefault true;
+		desktop.input.rime.enable = lib.mkDefault true;
 		desktop.input.fcitx5.enable = mkInputEnable "fcitx5";
+		desktop.input.ibus.enable = mkInputEnable "ibus";
 	};
 }
