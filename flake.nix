a4af86d1409102d6a3f934d4df5f107b0dec878b
Your Name <you@example.com>
2026-07-31 03:41:12 +0800

    up

--- flake.nix
+++ flake.nix
@@ -23,11 +23,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     noctalia-greeter = {
       url = "github:noctalia-dev/noctalia-greeter";
       inputs.nixpkgs.follows = "nixpkgs";
