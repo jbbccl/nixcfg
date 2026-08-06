3f5dd528f7a62fa30dce4f4eda2c727535012b43
Your Name <you@example.com>
2026-08-06 16:43:16 +0800

    e

--- core/user.nix
+++ core/user.nix
@@ -4,8 +4,7 @@
   username,
   ...
 }: {
-  # 最低优先级: option default, 无其他定义时才生效
-  # 主机覆盖: configuration.nix 里写 core.user.username = "foo";
+
   options.core.user.username = lib.mkOption {
     type = lib.types.str;
     default = "e";
@@ -13,7 +12,6 @@
   };
 
   config = {
-    # 桥: 解析后的值注入模块参数, 模块头 { username, ... } 保持零改动
     _module.args.username = config.core.user.username;
 
     users.users.${username} = {
